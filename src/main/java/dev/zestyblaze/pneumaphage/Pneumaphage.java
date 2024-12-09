5ef32b9366f7a810428d5f85b8e2edfd35849a08
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2024-12-09 21:58:00 +0000

    Initial Push

---
+++ src/main/java/dev/zestyblaze/pneumaphage/Pneumaphage.java
@@ -0,0 +1,12 @@
+package dev.zestyblaze.pneumaphage;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Pneumaphage.MODID)
+public class Pneumaphage {
+    public static final String MODID = "pneumaphage";
+
+    public Pneumaphage(IEventBus modEventBus, ModContainer modContainer) {}
+}
